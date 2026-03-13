f59910b4616cd00a884520880d79391a5696bc0b
Joan Segura <jsegurmor@gmail.com>
2026-03-13 13:27:04 -0700

    Revert "no sorting for esm from structure"
    This reverts commit 60e5a230ff71b60bb28800f0f5769590fe8a7445.

--- src/rcsb_embedding_model/dataset/esm_prot_from_structure.py
+++ src/rcsb_embedding_model/dataset/esm_prot_from_structure.py
@@ -49,6 +49,7 @@ class EsmProtFromStructure(IterableDataset):
             dtype=str,
             names=EsmProtFromStructure.COLUMNS
         )
+        self.data = self.data.sort_values(by=self.data.columns[0])
 
     def __iter__(self):
         # Handle multiple workers by splitting data across workers
